0307b0b0282fdbdffbc03adcb9e46f34d4768b6a
MMilton1 <murraylmilton@outlook.com>
2025-01-23 12:52:57 +0100

    created functions for game parts to make code more modular

--- Exercises/RockPaperScissor/main.py
+++ Exercises/RockPaperScissor/main.py
@@ -7,12 +7,24 @@ print("Let's play RockPaperScissors.")
 for images in game_image:
     print(images)
 
-computer_choice = random.choice(game_image)
-try:
-    user_pick = int(
-        input("Enter your choice:\n0 - for Rock\n1 - for Paper\n2 - for Scissors")
-    )
-    user_choice = game_image[user_pick]
+
+def get_user_choice():
+    while True:
+        user_pick = int(
+            input("Enter your choice:\n0 - for Rock\n1 - for Paper\n2 - for Scissors\n")
+        )
+        if game_image[user_pick] in game_image:
+            return game_image[user_pick]
+        else:
+            print("Invalid choice")
+
+
+def get_choices(computer_choice, user_choice):
+    computer_choice = random.choice(game_image)
+    user_choice = get_user_choice()
+
+
+def get_winner(computer_choice, user_choice):
     if computer_choice == "rock" and user_choice == "paper":
         print(f"Computer Wins! with\n{computer_choice}.\nYou chose:\n{user_choice}")
     elif computer_choice == "paper" and user_choice == "rock":
@@ -24,7 +36,24 @@ try:
         print(f"Computer:\n{computer_choice}.\nYou:\n{user_choice}")
     else:
         print(f"You win! with:\n{user_choice}. Computer chose: \n{computer_choice}")
-except IndexError:
-    print("Out of range of 0 - 2. Game Over.")
-except ValueError:
-    print("Invalid input. Game Over.")
+
+
+def play_game():
+    while True:
+        try:
+
+            computer_choice = random.choice(game_image)
+            user_choice = get_user_choice()
+
+            get_winner(computer_choice, user_choice)
+
+        except IndexError:
+            print("Out of range of 0 - 2. Game Over.")
+        except ValueError:
+            print("Invalid input. Game Over.")
+        continue_playing = input("Continue? (y/n): ").lower()
+        if continue_playing == "n":
+            break
+
+
+play_game()
